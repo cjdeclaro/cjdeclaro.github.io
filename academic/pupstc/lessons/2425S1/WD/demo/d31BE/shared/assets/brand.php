f3743f2c932a344d7b60c7906d553b83a25c7326
cjdeclaro <cjdeclaro16@gmail.com>
2024-12-15 13:28:23 +0800

    Demos

---
+++ academic/pupstc/lessons/2425S1/WD/demo/d31BE/shared/assets/brand.php
@@ -0,0 +1,21 @@
+<style>
+  .poweredby {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
+
+  .poweredby img {
+    width: 40px;
+  }
+
+  .poweredby .me-2 {
+    font-size: 10px;
+  }
+</style>
+
+<div class="poweredby d-flex flex-row align-items-end justify-content-end p-3">
+  <div class="me-2">
+    Powered by the Computer Society
+  </div>
+</div>
